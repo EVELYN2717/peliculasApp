e3205f3401e41f918c45de2268dafd0a7f75d540
Evelyn <evelynt.gutierrez@gmail.com>
2024-04-24 18:03:28 -0500

    feat: maquetar pantalla detalles peliculas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { PeliculasComponent } from './pages/peliculas/peliculas.component';
 import { ListaResultadosComponent } from './pages/lista-resultados/lista-resultados.component';
+import { DetallesPeliculaComponent } from './pages/detalles-pelicula/detalles-pelicula.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/peliculas', pathMatch: 'full'},
     {path: 'peliculas', component: PeliculasComponent},
     {path: '', redirectTo: '/lista-resultados', pathMatch: 'full'},
-    {path: 'lista-resultados', component: ListaResultadosComponent}
+    {path: 'lista-resultados', component: ListaResultadosComponent},
+    {path: '', redirectTo: '/detalles-pelicula', pathMatch: 'full'},
+    {path: 'detalles-pelicula', component: DetallesPeliculaComponent}
 ];
--- src/app/pages/detalles-pelicula/detalles-pelicula.component.ts
+++ src/app/pages/detalles-pelicula/detalles-pelicula.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-detalles-pelicula',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './detalles-pelicula.component.html',
   styleUrl: './detalles-pelicula.component.scss'
 })
