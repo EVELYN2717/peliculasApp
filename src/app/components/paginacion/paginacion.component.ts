09f45063790c9f6522dddd0c44abb32b8fb16394
Evelyn <evelynt.gutierrez@gmail.com>
2024-04-24 15:00:02 -0500

    feat: maquetar componente paginacion

--- src/app/components/paginacion/paginacion.component.ts
+++ src/app/components/paginacion/paginacion.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
+import { Subject } from 'rxjs';
+
+// @Injectable()
+// export class MyCustomPaginatorIntl implements MatPaginatorIntl {
+//   changes = new Subject<void>();
+
+//   // firstPageLabel = `First page`;
+//   // itemsPerPageLabel = ``;
+//   // lastPageLabel = `Last page`;
+//   // nextPageLabel = 'Next page';
+//   // previousPageLabel = 'Previous page';
+
+//   getRangeLabel(page: number, pageSize: number, length: number): string {
+//     if (length === 0) {
+//       return `Page 1 of 1`;
+//     }
+//     const amountPages = Math.ceil(length / pageSize);
+//     return `Page ${page + 1} of ${amountPages}`;
+//   }
+// }
 
 @Component({
   selector: 'app-paginacion',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule],
+  providers: [{provide: MatPaginatorIntl
+    // , useClass: MyCustomPaginatorIntl
+  }],
   templateUrl: './paginacion.component.html',
   styleUrl: './paginacion.component.scss'
 })
--- src/app/pages/lista-resultados/lista-resultados.component.ts
+++ src/app/pages/lista-resultados/lista-resultados.component.ts
@@ -1,34 +1,13 @@
-import { Component, Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
-import { Subject } from 'rxjs';
+import { PaginacionComponent } from '../../components/paginacion/paginacion.component';
 
-// @Injectable()
-// export class MyCustomPaginatorIntl implements MatPaginatorIntl {
-//   changes = new Subject<void>();
-
-//   // firstPageLabel = `First page`;
-//   // itemsPerPageLabel = ``;
-//   // lastPageLabel = `Last page`;
-//   // nextPageLabel = 'Next page';
-//   // previousPageLabel = 'Previous page';
-
-//   getRangeLabel(page: number, pageSize: number, length: number): string {
-//     if (length === 0) {
-//       return `Page 1 of 1`;
-//     }
-//     const amountPages = Math.ceil(length / pageSize);
-//     return `Page ${page + 1} of ${amountPages}`;
-//   }
-// }
 
 @Component({
   selector: 'app-lista-resultados',
   standalone: true,
-  imports: [HeaderComponent, MatPaginatorModule],
-  providers: [{provide: MatPaginatorIntl
-    // , useClass: MyCustomPaginatorIntl
-  }],
+  imports: [HeaderComponent, PaginacionComponent],
+  providers: [],
   templateUrl: './lista-resultados.component.html',
   styleUrl: './lista-resultados.component.scss'
 })
