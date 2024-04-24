569fa793a3283865aae6c488942f730faef84720
Evelyn <evelynt.gutierrez@gmail.com>
2024-04-24 14:49:45 -0500

    feat: maquetar pagina de lista de resultados de busqueda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { PeliculasComponent } from './pages/peliculas/peliculas.component';
+import { ListaResultadosComponent } from './pages/lista-resultados/lista-resultados.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/peliculas', pathMatch: 'full'},
-    {path: 'peliculas', component: PeliculasComponent}
+    {path: 'peliculas', component: PeliculasComponent},
+    {path: '', redirectTo: '/lista-resultados', pathMatch: 'full'},
+    {path: 'lista-resultados', component: ListaResultadosComponent}
 ];
--- src/app/pages/lista-resultados/lista-resultados.component.ts
+++ src/app/pages/lista-resultados/lista-resultados.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
+import { Subject } from 'rxjs';
+
+// @Injectable()
+// export class MyCustomPaginatorIntl implements MatPaginatorIntl {
+//   changes = new Subject<void>();
+
+//   // firstPageLabel = `First page`;
+//   // itemsPerPageLabel = ``;
+//   // lastPageLabel = `Last page`;
+//   // nextPageLabel = 'Next page';
+//   // previousPageLabel = 'Previous page';
+
+//   getRangeLabel(page: number, pageSize: number, length: number): string {
+//     if (length === 0) {
+//       return `Page 1 of 1`;
+//     }
+//     const amountPages = Math.ceil(length / pageSize);
+//     return `Page ${page + 1} of ${amountPages}`;
+//   }
+// }
 
 @Component({
   selector: 'app-lista-resultados',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MatPaginatorModule],
+  providers: [{provide: MatPaginatorIntl
+    // , useClass: MyCustomPaginatorIntl
+  }],
   templateUrl: './lista-resultados.component.html',
   styleUrl: './lista-resultados.component.scss'
 })
